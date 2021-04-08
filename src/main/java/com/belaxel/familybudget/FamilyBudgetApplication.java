b1d233632c074fd5aeb09bd803d3a34f4486cb59
Aleksei Belousov <belaxel@mail.ru>
2021-04-08 22:22:25 +0500

    Initial commit

---
+++ src/main/java/com/belaxel/familybudget/FamilyBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.belaxel.familybudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belaxel/familybudget/FamilyBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belaxel.familybudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
